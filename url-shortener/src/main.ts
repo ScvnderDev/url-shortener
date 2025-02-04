cf1d0b599187622ca144f4029fa6969f60edf8b7
Skander Lassoued <skander.lassoued@steps.tn>
2025-02-04 15:42:46 +0100

    deploy config

--- url-shortener/src/main.ts
+++ url-shortener/src/main.ts
@@ -20,6 +20,8 @@ async function bootstrap() {
   SwaggerModule.setup('/docs', app, documentFactory);
   app.use(cookieParser());
 
-  await app.listen(3000, () => console.log('Server running ...'));
+  await app.listen(process.env.PORT || 3000, () =>
+    console.log('Server running ...'),
+  );
 }
 bootstrap();
