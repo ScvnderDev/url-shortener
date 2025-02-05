4fb34467844b60d6e1b39bef8163022de44d8075
Skander Lassoued <lassoued.skander@outlook.com>
2025-02-05 14:00:28 +0000

    fixx import

--- url-shortener/src/app.controller.ts
+++ url-shortener/src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get, Res } from "@nestjs/common";
-import { HealthCheck, HealthCheckService, HttpHealthIndicator } from "@nestjs/terminus";
 import { Response } from "express";
 import { join } from "path";
 
 @Controller()
 export class AppController {
-  constructor(private health: HealthCheckService,
-    private http: HttpHealthIndicator){}
+  constructor(){}
   @Get('*')
   serveClient(@Res() res: Response) {
     res.sendFile(join(__dirname, '..', 'public', 'index.html'));
