5db59217f85687f78d6d1ee9cba9b3817ec30e3e
Skander Lassoued <lassoued.skander@outlook.com>
2025-02-05 13:32:19 +0000

    add deps 2

--- url-shortener/src/user/user.module.ts
+++ url-shortener/src/user/user.module.ts
@@ -5,13 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { HealthCheckService } from '@nestjs/terminus';
+import { HealthCheckExecutor } from '@nestjs/terminus/dist/health-check/health-check-executor.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UserController],
-  providers: [UserService, JwtService,HealthCheckService ],
+  providers: [UserService, JwtService,HealthCheckService ,HealthCheckExecutor ],
   exports: [UserService],
 })
 export class UserModule {}
